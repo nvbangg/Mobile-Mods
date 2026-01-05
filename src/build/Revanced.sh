137d9ccb58c12afa7c16b02bd33568728a23a274
nvbangg <nvbangg@gmail.com>
2026-01-05 11:41:58 +0700

    only arm64

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -10,24 +10,24 @@ revanced_dl(){
 	# Patch YouTube:
 	get_patches_key "youtube-revanced"
 	get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	split_editor "youtube" "youtube"
-	patch "youtube" "revanced"
+	# split_editor "youtube" "youtube"
+	# patch "youtube" "revanced"
 	# Patch Youtube Arm64-v8a
 	get_patches_key "youtube-revanced" 
 	split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "youtube-arm64-v8a" "revanced"
-	# Patch Youtube Armeabi-v7a
-	get_patches_key "youtube-revanced" 
-	split_editor "youtube" "youtube-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
-	patch "youtube-armeabi-v7a" "revanced"
-	# Patch Youtube x86
-	get_patches_key "youtube-revanced" 
-	split_editor "youtube" "youtube-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
-	patch "youtube-x86" "revanced"
-	# Patch Youtube x86_64
-	get_patches_key "youtube-revanced" 
-	split_editor "youtube" "youtube-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
-	patch "youtube-x86_64" "revanced"
+	# # Patch Youtube Armeabi-v7a
+	# get_patches_key "youtube-revanced" 
+	# split_editor "youtube" "youtube-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
+	# patch "youtube-armeabi-v7a" "revanced"
+	# # Patch Youtube x86
+	# get_patches_key "youtube-revanced" 
+	# split_editor "youtube" "youtube-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
+	# patch "youtube-x86" "revanced"
+	# # Patch Youtube x86_64
+	# get_patches_key "youtube-revanced" 
+	# split_editor "youtube" "youtube-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
+	# patch "youtube-x86_64" "revanced"
 }
 2() {
 	revanced_dl
@@ -151,18 +151,18 @@ revanced_dl(){
 	get_patches_key "youtube-music-revanced"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 	patch "youtube-music-arm64-v8a" "revanced"
-	# Armeabi-v7a
-	get_patches_key "youtube-music-revanced"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
-	patch "youtube-music-armeabi-v7a" "revanced"
-	# x86_64
-	get_patches_key "youtube-music-revanced"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
-	patch "youtube-music-x86_64" "revanced"
-	# x86
-	get_patches_key "youtube-music-revanced"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
-	patch "youtube-music-x86" "revanced"
+	# # Armeabi-v7a
+	# get_patches_key "youtube-music-revanced"
+	# get_apk "com.google.android.apps.youtube.music" "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
+	# patch "youtube-music-armeabi-v7a" "revanced"
+	# # x86_64
+	# get_patches_key "youtube-music-revanced"
+	# get_apk "com.google.android.apps.youtube.music" "youtube-music-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
+	# patch "youtube-music-x86_64" "revanced"
+	# # x86
+	# get_patches_key "youtube-music-revanced"
+	# get_apk "com.google.android.apps.youtube.music" "youtube-music-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
+	# patch "youtube-music-x86" "revanced"
 }
 10() {
 	revanced_dl
